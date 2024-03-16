e5653c730ae3fa7c4f8a8bc96c62a88945b4fd15
aliOrifjonovich <orifjonovichalisher@gmail.com>
2024-03-16 13:54:31 +0500

    Database is connected

---
+++ WAD_BACKEND_14610/Data/EventManagementDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WAD_BACKEND_14610.Data
+{
+    public class EventManagementDbContext : DbContext
+    {
+        public EventManagementDbContext(DbContextOptions<EventManagementDbContext> options) : base(options) { }
+    }
+}
--- WAD_BACKEND_14610/Program.cs
+++ WAD_BACKEND_14610/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WAD_BACKEND_14610.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<EventManagementDbContext>(
+    e => e.UseSqlServer(builder.Configuration.GetConnectionString("EventManagementConnectionString"))
+    );
 
 var app = builder.Build();
 
